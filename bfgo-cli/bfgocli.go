620d2b44abc75eb2c58012e83dc8bb389e708edd
Dean Bassett <deanvelopermn@gmail.com>
2017-05-08 18:54:42 -0500

    added defaults

--- bfgo-cli/bfgocli.go
+++ bfgo-cli/bfgocli.go
@@ -12,19 +12,19 @@ import (
 )
 
 type options struct {
-    EOFNoChange    bool `short:"n" long:"eofnochange" description:"Decides if \",\" should change a cell on EOF. Overrides -d"`
-    EOFDefault     byte `short:"d" long:"eofdefault" description:"Decides what \",\" should set a cell to on EOF."`
-    KeepCR         bool `short:"c" long:"keepcr" description:"Decides if CR should be kept in CRLF linebreaks"`
-    InitialArrSize uint64 `short:"s" long:"initialarrsize" description:"Initial size for the tape"`
-    Input          string `short:"i" long:"input" description:"Input source. \"stdin\" for cli input, \"!\" for BF input, otherwise name of a file."`
-    Output         string `short:"o" long:"output" description:"Output destination. \"stdout\" for cli output, otherwise a filename."`
+    EOFNoChange    bool `short:"n" long:"eofnochange" default:"true" description:"Decides if \",\" should change a cell on EOF. Overrides -d"`
+    EOFDefault     byte `short:"d" long:"eofdefault" default:"10" description:"Decides what \",\" should set a cell to on EOF."`
+    KeepCR         bool `short:"c" long:"keepcr" default:"false" description:"Decides if CR should be kept in CRLF linebreaks"`
+    InitialArrSize uint64 `short:"s" long:"initialarrsize" default:"30" description:"Initial size for the tape"`
+    Input          string `short:"i" long:"input" default:"stdin" description:"Input source. \"stdin\" for cli input, \"!\" for BF input, otherwise name of a file."`
+    Output         string `short:"o" long:"output" default:"stdout" description:"Output destination. \"stdout\" for cli output, otherwise a filename."`
 }
 
 func main() {
     opts := options{}
     args, err := flags.Parse(&opts)
     if err != nil {
-        panic(err)
+        fmt.Fprintf(os.Stderr, "Error: %s\n", err)
     }
 
     var bf []byte
@@ -56,7 +56,7 @@ func main() {
         input = file
     }
 
-    if opts.Output == "stdout" {
+    if opts.Output == "" || opts.Output == "stdout" {
         output = os.Stdout
     } else {
         file, err := os.Open(opts.Output)
